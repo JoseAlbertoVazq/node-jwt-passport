93032761e8a07b5250a6a57c07314762ec7967bd
Jose Alberto Vázquez <javazquez@clever-global.com>
2019-12-02 13:33:52 +0100

    added bcrypt logic to user's password

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -3,6 +3,8 @@ const User = require('../models').User;
 const Op = Sequelize.Op;
 const jwt = require('jsonwebtoken');
 const secret = 'sequelizExpress';
+const bcrypt = require('bcrypt');
+const SALT_NUMBER = 10;
 module.exports = {
     getUsers(req, res) {
         return User.findAll()
@@ -28,24 +30,29 @@ module.exports = {
                 ]
             }
         }).then((found) => {
+
             if (found) {
                 return res.sendStatus(409);
             }
             if (req.body.password !== req.body.confirmPassword) {
                 return res.sendStatus(400);
             }
-            User.create({
-                username: req.body.username,
-                password: req.body.password,
-                email: req.body.email,
-                firstName: req.body.firstName,
-                lastName: req.body.lastName
-            }).then((created) => {
-                return res.status(201).send({
-                    "message": "User created!",
-                    "details": created
-                });
-            }).catch(() => res.sendStatus(500));
+            // Before creating the user in our db, let's bcrypt it!
+            bcrypt.hash(req.body.password, SALT_NUMBER, (err, hash) => {
+                if (err || hash == undefined) return res.sendStatus(500);
+                User.create({
+                    username: req.body.username,
+                    password: hash,
+                    email: req.body.email,
+                    firstName: req.body.firstName,
+                    lastName: req.body.lastName
+                }).then((created) => {
+                    return res.status(201).send({
+                        "message": "User created!",
+                        "details": created
+                    });
+                }).catch((error) => res.status(500).send(error));
+            })
         }).catch(() => res.sendStatus(500));
     },
 
@@ -53,19 +60,25 @@ module.exports = {
         if (!req.body.username || !req.body.password) {
             return res.sendStatus(400);
         }
-        User.findOne({
+        return User.findOne({
             where: { username: req.body.username }
         }).then((user) => {
-            if (user.password === req.body.password) {
-                let payload = { id: user.id };
-                let token = jwt.sign(payload, secret);
-                // Now we can save this token with Express, so we can call res.locals.token
-                // anywhere in our backend
-                res.locals.token = token;
-                return res.status(200).send({ "message": "Login successful", "token": token });
-            } else {
-                return res.sendStatus(400);
-            }
+            if (!user) return res.sendStatus(400);
+            // Compare the password attribute of the body request with
+            // bcrypted password stored in db
+            bcrypt.compare(req.body.password, user.password, ((err, result) => {
+                if (err) return res.sendStatus(500);
+                if (result) {
+                    let payload = { id: user.id };
+                    let token = jwt.sign(payload, secret);
+                    // Now we can save this token with Express, so we can call res.locals.token
+                    // anywhere in our backend
+                    res.locals.token = token;
+                    return res.status(200).send({ "message": "Login successful", "token": token });
+                } else {
+                    return res.sendStatus(400);
+                }
+            }))
         }).catch(() => res.sendStatus(404));
     },
     // helper method for jwt
